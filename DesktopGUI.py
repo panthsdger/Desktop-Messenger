270db11a8b1ef6767c8c61f540490a3f3d1cc8be
panthsdger <panthsdger@aol.com>
2017-04-15 21:46:21 -0400

    Update DesktopGUI.py
    basic gui implementation

--- DesktopGUI.py
+++ DesktopGUI.py
@@ -1,1 +1,14 @@
+import random
+from appJar import gui
+def none():
+    1 == 1
 
+frame = gui()
+frame.addLabel("user", "Username: ")
+frame.setLabelBg("user", "gray")
+frame.addEntry("userdata")
+frame.addLabel("pass", "Password: ")
+frame.setLabelBg("pass", "gray")
+frame.addSecretEntry("passdata")
+frame.addButton("Submit", none())
+frame.go()
