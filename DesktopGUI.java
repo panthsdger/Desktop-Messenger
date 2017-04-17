5ff2919ae75deb4df143b3521e606863855ff200
panthsdger <panthsdger@aol.com>
2017-04-17 17:38:17 -0400

    Update DesktopGUI.java

--- DesktopGUI.java
+++ DesktopGUI.java
@@ -1,12 +1,7 @@
- import javax.swing.JFrame;
- import javax.swing.JPanel;
- import javax.swing.JComboBox;
- import javax.swing.JButton;
- import javax.swing.JLabel;
- import javax.swing.JList;
- import java.awt.*;
- import java.awt.event.ActionListener;
- import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Desktop_GUI extends JFrame{
 
