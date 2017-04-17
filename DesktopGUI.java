e9f3ba0eac1dbe81f3c7f2bf63c5984dc07b5c08
panthsdger <panthsdger@aol.com>
2017-04-17 17:16:30 -0400

    Update DesktopGUI.java
    made a grid layout

--- DesktopGUI.java
+++ DesktopGUI.java
@@ -1,54 +1,68 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Desktop_GUI {
-    int addup = 0;
+public class Desktop_GUI extends JFrame{
+
+
     public static void main(String[] args){
-        new Desktop_GUI();
+        Desktop_GUI desk_gui = new Desktop_GUI("test", new Dimension(800, 600));
     }
 
-    public Desktop_GUI(){
-        //SETTING UP MAIN FRAME
-        JFrame guiFrame = new JFrame();
-        guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        guiFrame.setTitle("generic_gui");
-        guiFrame.setSize(800,600);
-        guiFrame.setLocationRelativeTo(null);
-        guiFrame.setVisible(true);
-        guiFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    private Desktop_GUI(String title, Dimension size) {
+        JFrame frame = mainFrame(title, size);
+    }
+
+    private JFrame mainFrame(String title, Dimension size){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(size);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setBackground(Color.darkGray);
+        frame.getContentPane().setLayout(new GridBagLayout());
+        //need to alter gridbag before
+//      frame.getContentPane().add(mainPanel(new Dimension(600, 400)), new Grid);
+
+        frame.add(chatter(new Dimension(800, 600)));
 
 
+        frame.pack();
+
+        return frame;
+    }
+
+
+
+    private JPanel chatter(Dimension sizeScale){
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(15,15,15,15);
+
+        JPanel chatroom = new JPanel(new GridBagLayout());
+
+        chatroom.setPreferredSize(sizeScale);
+        chatroom.setLocation(300, 300);
+
+        JTextArea textArea = new JTextArea("text area");
+        JTextField textField = new JTextField("text field");
+
+        chatroom.add(textArea, gbc);
+        chatroom.add(textField, gbc);
+
         
-        
-        // FRAMES ANd BUTTONS FOR CLICKING
-        JPanel buttonPanel = new JPanel();
-        JButton resetButton = new JButton();
-        JButton countButton = new JButton();
-        countButton.setText(""+addup);
-        resetButton.setText("Reset Count");
-        buttonPanel.add(countButton);
-        buttonPanel.add(resetButton);
-        countButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                addup++;
-                countButton.setText(""+addup);
-            }
-        });
-        resetButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                addup = 0;
-                countButton.setText("0");
-            }
-        });
-        guiFrame.add(buttonPanel);
+        return chatroom;
+    }
+
+
+    private JPanel mainPanel(Dimension frame){
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        mainPanel.add(chatter(frame));
+
+        return mainPanel;
     }
 }
+
+
